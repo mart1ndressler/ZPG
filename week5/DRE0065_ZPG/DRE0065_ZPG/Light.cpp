6556f937d753075306ce5b4a49b889ede4722a92
Martin Dressler <m.dressler321@gmail.com>
2025-10-20 14:27:51 +0200

    Completed
    Removed methods for attaching, detaching, and pushing light data to shader programs.

--- week5/DRE0065_ZPG/DRE0065_ZPG/Light.cpp
+++ week5/DRE0065_ZPG/DRE0065_ZPG/Light.cpp
@@ -10,32 +10,4 @@ vec3 Light::getPosition() const
 void Light::setPosition(const vec3& p)
 {
     position = p;
-    pushToPrograms();
-}
-
-void Light::attach(ShaderProgram* program)
-{
-    if(!program) return;
-    if(find(programs.begin(), programs.end(), program) == programs.end())
-    {
-        programs.push_back(program);
-        pushTo(program);
-    }
-}
-
-void Light::detach(ShaderProgram* program)
-{
-    auto it = find(programs.begin(), programs.end(), program);
-    if(it != programs.end()) programs.erase(it);
-}
-
-void Light::pushToPrograms() const
-{
-    for(auto* p : programs) if(p) pushTo(p);
-}
-
-void Light::pushTo(ShaderProgram* program) const
-{
-    program->use();
-    program->set("lightPosition", position);
 }
